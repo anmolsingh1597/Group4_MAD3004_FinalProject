3c94ac58eef93b53564947f9215b75d57b08d0f8
Aman Kaur <amankaur2628@gmail.com>
2020-02-12 17:34:22 -0500

    [Aman]Person class created

--- Group4_MAD3004_FinalProject/Person.swift
+++ Group4_MAD3004_FinalProject/Person.swift
@@ -1,11 +1,13 @@
 //
-//  main.swift
+//  Person.swift
 //  Group4_MAD3004_FinalProject
 //
-//  Created by Anmol singh on 2020-02-12.
+//  Created by MacStudent on 2020-02-12.
 //  Copyright © 2020 Swift Project. All rights reserved.
 //
 
 import Foundation
 
-
+public class Person{
+    
+}
