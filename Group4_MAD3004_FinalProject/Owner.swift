c8340136373b9544c7330a7432c6a0f086ccd638
Aman Kaur <amankaur2628@gmail.com>
2020-02-12 18:00:07 -0500

    [Aman]Class Owner: Variables declared

--- Group4_MAD3004_FinalProject/Owner.swift
+++ Group4_MAD3004_FinalProject/Owner.swift
@@ -15,11 +15,6 @@ public class Owner: Person
     var website: String?
     //var vehicleList:
     
-    init(id: Int, firstName: String, lastName: String, gender: Enum.Gender, birthDate: Date, mobileNumber: String, email: String, userName: String, password: String, companyTitle: String?, businessNumber: String?, website: String?)
-    {
-        super.init(id: id, firstName: firstName, lastName: lastName, gender: gender, birthDate: birthDate, mobileNumber: mobileNumber, email: email, userName: userName, password: password)
-        self.companyTitle = companyTitle
-        self.businessNumber = businessNumber
-        self.website = website
-    }
+  
 }
+
