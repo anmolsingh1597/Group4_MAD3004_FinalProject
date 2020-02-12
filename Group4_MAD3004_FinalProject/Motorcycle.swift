1fec4e0d19023c09dbaedca55c1df80885ed2f3d
Aman Kaur <amankaur2628@gmail.com>
2020-02-12 18:25:11 -0500

    [Aman] Class Motorcycle: Created

---
+++ Group4_MAD3004_FinalProject/Motorcycle.swift
@@ -0,0 +1,14 @@
+//
+//  Motorcycle.swift
+//  Group4_MAD3004_FinalProject
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
+
+public class Motorcycle
+{
+    
+}
