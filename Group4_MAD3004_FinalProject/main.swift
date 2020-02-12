2d84a018a9492b0b2b7f2942c838bc7203a49ce9
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-12 17:25:45 -0500

    Initial Commit

---
+++ Group4_MAD3004_FinalProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group4_MAD3004_FinalProject
+//
+//  Created by Anmol singh on 2020-02-12.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
