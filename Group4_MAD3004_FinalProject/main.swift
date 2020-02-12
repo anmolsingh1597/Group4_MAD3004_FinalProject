aa1e96c13a9bdbf22cd704f85398ab93b8f2635c
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-12 18:28:44 -0500

    [Anmol Singh] Testing: Person Object p1 created

--- Group4_MAD3004_FinalProject/main.swift
+++ Group4_MAD3004_FinalProject/main.swift
@@ -8,4 +8,6 @@
 
 import Foundation
 
+var p1 = Person(id: 24500, firstName: "Anmol", lastName: "Singh", gender: .Male, birthDate: Date(), mobileNumber: "87878788", email: "me@me.com", userName: "Me_Anmol", password: "DFRGFDF")
+
 
