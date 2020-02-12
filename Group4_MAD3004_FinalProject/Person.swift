b7684a21244ceacb7e8d73972391a9c5b29cd6ac
Aman Kaur <amankaur2628@gmail.com>
2020-02-12 17:40:00 -0500

    [Aman] Class Person : Variables added and Init declared

--- Group4_MAD3004_FinalProject/Person.swift
+++ Group4_MAD3004_FinalProject/Person.swift
@@ -8,6 +8,35 @@
 
 import Foundation
 
-public class Person{
-    
+public class Person
+{
+        var id : Int
+         var firstName : String
+         var lastName : String
+         var gender : Enum.Gender
+         var birthDate: Date?
+        
+         let now = Date()
+         
+         var age: Int{
+             return 0
+         }
+         var mobileNumber : String
+         var emailId : String
+         var userName : String
+         var password : String
+         
+         init(id : Int, firstName: String, lastName : String, gender : Enum.Gender, birthDate: Date, mobileNumber: String,
+              email: String, userName: String, password: String){
+             
+             self.id = id
+             self.firstName = firstName
+             self.lastName = lastName
+             self.gender = gender
+             self.birthDate = birthDate
+             self.mobileNumber = mobileNumber
+             self.emailId = email
+             self.userName = userName
+             self.password = password
+         }
 }
