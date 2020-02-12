08f1aea292a1a66d20ed853e4e9d7a98c40b1248
Aman Kaur <amankaur2628@gmail.com>
2020-02-12 17:36:53 -0500

    [Aman]Created Enum Class:Gender

---
+++ Group4_MAD3004_FinalProject/Enum/Enum.swift
@@ -0,0 +1,19 @@
+//
+//  Enum.swift
+//  Group4_MAD3004_FinalProject
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
+
+public struct Enum
+{
+    enum Gender {
+        case Male
+        case Female
+        case Other
+    }
+}
+
