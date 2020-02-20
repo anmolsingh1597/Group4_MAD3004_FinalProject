d0db86e8b8b5d096bcb2589d4be63de04321094c
Aman Kaur <amankaur2628@gmail.com>
2020-02-20 18:40:10 -0500

    [Aman] Extension String: EmailValidation

--- Group4_MAD3004_FinalProject/Extensions/String.swift
+++ Group4_MAD3004_FinalProject/Extensions/String.swift
@@ -15,4 +15,11 @@ extension String{
         return "\(firstName) \(lastName)"
     }
     
+    func emailValidation(email: String) -> Bool {
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: email)
+    }
+    
 }
