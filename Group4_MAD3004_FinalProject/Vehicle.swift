724517748a1a11af63c7de7558a544487a909be9
Aman Kaur <amankaur2628@gmail.com>
2020-02-20 19:09:40 -0500

    [Aman] Class Vehicle: Variables and Init updated

--- Group4_MAD3004_FinalProject/Vehicle.swift
+++ Group4_MAD3004_FinalProject/Vehicle.swift
@@ -10,15 +10,29 @@ import Foundation
     
     public class Vehicle{
       var vehicleIdentificationNumber: String
-      var vehicleDescription: String
-      var manufacturerName: String
+         var vehicleDescription: String
+         var manufacturerName: String
+         var isSelfDrive: Bool
+         var isInsured: Bool
+         var noOfSeat: Int
+         var prefixDetails: String
+         var insuranceProviderName: String
+         var baseRate: Int
+         var ratePerKm: Int
       init(vehicleIdentificationNumber: String,
          vehicleDescription: String,
-         manufacturerName: String)
+         manufacturerName: String, isSelfDrive : Bool, isInsured : Bool, noOfSeat : Int, prefixDetails : String, insuranceProviderName : String, baseRate : Int, ratePerKm : Int)
       {
         self.vehicleIdentificationNumber = vehicleIdentificationNumber
         self.vehicleDescription = vehicleDescription
         self.manufacturerName = manufacturerName
+        self.isSelfDrive = isSelfDrive
+        self.isInsured = isInsured
+        self.noOfSeat = noOfSeat
+        self.prefixDetails = prefixDetails
+        self.insuranceProviderName = insuranceProviderName
+        self.baseRate = baseRate
+        self.ratePerKm = ratePerKm
       }
     }
     
