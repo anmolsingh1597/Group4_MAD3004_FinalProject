99d948f5b4e69c19c7d8d01002400c2a50d56f6c
Aman Kaur <amankaur2628@gmail.com>
2020-02-20 19:04:23 -0500

    [Aman] Class Bus: Variables declared

--- Group4_MAD3004_FinalProject/Bus.swift
+++ Group4_MAD3004_FinalProject/Bus.swift
@@ -10,5 +10,18 @@ import Foundation
 
 public class Bus
 {
-    
+    var identificationNumber: String
+    var description: String
+    var isSelfDrive: Bool
+    var manufacturerName: String
+    var isInsured: Bool
+    var vehicleNoOfSeats: Int
+    var insuranceProviderName: String
+    var seat: Int
+    var fuelType: String
+    var ratePerDay: Double
+    var ratePerKm: Double
+    var busType: String
+    var iswifiAvailable: Bool
+    var isAccessibiltyServiceAvailable: Bool
 }
