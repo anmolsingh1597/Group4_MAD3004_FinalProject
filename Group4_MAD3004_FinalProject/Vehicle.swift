29668c19fc1fee15b3ac265a179a831fba7effc0
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-22 11:41:29 -0500

    [Anmol Singh] Class Vehicle: Display Delegate override

--- Group4_MAD3004_FinalProject/Vehicle.swift
+++ Group4_MAD3004_FinalProject/Vehicle.swift
@@ -34,6 +34,10 @@ public class Vehicle: DisplayDelegate
         self.noOfSeat = noOfSeat
       }
         
+    func display() {
+        print("-----Vehicle-----")
+        
+    }
     }
     
 
