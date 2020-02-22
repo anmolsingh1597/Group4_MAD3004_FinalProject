13cff3266fcf18ad8f17743aaf175e20f4b3dff6
Aman Kaur <amankaur2628@gmail.com>
2020-02-22 12:36:47 -0500

    [Aman]Extension Int: Prefix added

---
+++ Group4_MAD3004_FinalProject/Extensions/Int+Extension.swift
@@ -0,0 +1,38 @@
+//
+//  Double+Extension.swift
+//  Group4_MAD3004_FinalProject
+//
+//  Created by Anmol singh on 2020-02-22.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
+
+extension Int{
+
+func concatCustomerPrefix() -> String
+{
+    return "CUS\(self)"
+}
+func concatDriverPrefix() -> String
+{
+    return "DRI\(self)"
+}
+func concatOwnerPrefix() -> String
+{
+    return "OWN\(self)"
+}
+func concatCarPrefix() -> String
+{
+     return "CAR\(self)"
+}
+func concatMotorcyclePrefix() -> String
+{
+    return "MOT\(self)"
+}
+func concatBusPrefix() -> String
+{
+    return "BUS\(self)"
+}
+    
+}
