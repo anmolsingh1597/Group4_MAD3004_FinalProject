96abebde5cda9e5a3a655157c8861f601368bfc9
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-22 11:54:37 -0500

    [Anmol Singh]Class Motorcycle: Display func overriden

--- Group4_MAD3004_FinalProject/Car.swift
+++ Group4_MAD3004_FinalProject/Car.swift
@@ -24,6 +24,7 @@ public class Car: Vehicle
         super.init(vehicleIdentificationNumber: vehicleIdentificationNumber, vehicleDescription: vehicleDescription, manufacturerName: manufacturerName, isSelfDrive: isSelfDrive, isInsured: isInsured, noOfSeat: noOfSeat, fuelType: fuelType)
     }
     override func display() {
+        print("-----Car-----")
         super.display()
         print("Car Type: \(self.carType)")
         print("Car Color: \(self.carColor)")
