389909c32816c03d709af169fcf86f75a3033945
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-22 11:51:02 -0500

    [Anmol Singh]Class Car: Display func overrides

--- Group4_MAD3004_FinalProject/Car.swift
+++ Group4_MAD3004_FinalProject/Car.swift
@@ -23,4 +23,9 @@ public class Car: Vehicle
         self.carColor = carColor
         super.init(vehicleIdentificationNumber: vehicleIdentificationNumber, vehicleDescription: vehicleDescription, manufacturerName: manufacturerName, isSelfDrive: isSelfDrive, isInsured: isInsured, noOfSeat: noOfSeat, fuelType: fuelType)
     }
+    override func display() {
+        super.display()
+        print("Car Type: \(self.carType)")
+        print("Car Color: \(self.carColor)")
+    }
 }
