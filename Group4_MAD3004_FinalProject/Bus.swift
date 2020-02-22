c1c56da3ea757ee3e270f7fd483954f77ba171db
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-22 11:32:28 -0500

    [Anmol Singh] Enum: BusType enum generated

--- Group4_MAD3004_FinalProject/Bus.swift
+++ Group4_MAD3004_FinalProject/Bus.swift
@@ -10,36 +10,7 @@ import Foundation
 
 public class Bus
 {
-    var identificationNumber: String
-    var description: String
-    var isSelfDrive: Bool
-    var manufacturerName: String
-    var isInsured: Bool
-    var vehicleNoOfSeats: Int
-    var insuranceProviderName: String
-    var seat: Int
-    var fuelType: String
-    var ratePerDay: Double
-    var ratePerKm: Double
-    var busType: String
-    var iswifiAvailable: Bool
-    var isAccessibiltyServiceAvailable: Bool
-    
-    init(identificationNumber: String, description: String, isSelfDrive: Bool, manufacturerName: String, isInsured: Bool, vehicleNoOfSeats: Int, insuranceProviderName: String, seat: Int, fuelType: String, ratePerDay: Double, ratePerKm: Double, busType : String , iswifiAvailable : Bool, isAccessibiltyServiceAvailable: Bool)
-    {
-        self.identificationNumber = identificationNumber
-        self.description = description
-        self.isSelfDrive = isSelfDrive
-        self.manufacturerName = manufacturerName
-        self.isInsured = isInsured
-        self.vehicleNoOfSeats = vehicleNoOfSeats
-        self.insuranceProviderName = insuranceProviderName
-        self.seat = seat
-        self.fuelType = fuelType
-        self.ratePerDay = ratePerDay
-        self.ratePerKm = ratePerKm
-        self.busType = busType
-        self.iswifiAvailable = iswifiAvailable
-    self.isAccessibiltyServiceAvailable = isAccessibiltyServiceAvailable
-}
+ var typeOfBus (Mini, Volvo, etc.)
+    -isAccessibilityServiceavailable
+    - is Wifi Available
 }
