5471270364db0d67bc9849342a2d3e95c90c578e
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-24 18:36:49 -0500

    [Anmol Singh]Extension: Motorcycle

---
+++ Group4_MAD3004_FinalProject/Extensions/Motorcycle+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  Motorcycle+Extension.swift
+//  Group4_MAD3004_FinalProject
+//
+//  Created by Anmol singh on 2020-02-24.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
+
+extension Motorcycle {
+    var dataDictionary: [String: Any] {
+    return [
+        "vin": self.vehicleIdentificationNumber,
+        "vehicleDescription": self.vehicleDescription,
+        "manufacturerName": self.manufacturerName,
+        "selfDrive": self.isSelfDrive,
+        "driver": self.driverName,
+        "isInsured": self.isInsured,
+        "insuranceProviderName": self.insuranceProviderName,
+        "noOfSeats": self.noOfSeat,
+        //"fuelType": self.fuelType,
+        "baseRate": self.baseRate.getFormattedAmount(),
+        "ratePerKm": self.ratePerKm.getFormattedAmount(),
+        "maxTopSpeed": self.maxTopSpeed,
+        "milage": self.milage
+    ];
+  }
+}
