5192329b75df2a37c211617d6ac9a0b032bb90e2
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-23 21:08:03 -0500

    [Anmol Singh] Class DataRepo: Class objects to JSON attempt 1

--- Group4_MAD3004_FinalProject/DataRepo.swift
+++ Group4_MAD3004_FinalProject/DataRepo.swift
@@ -10,31 +10,32 @@ import Foundation
 
 class DataRepo{
     
-   static private var repository = DataRepo()
-    
-    private lazy var vehicleList = Dictionary <String,Vehicle>()
-    
-    private init()
-    {
-        
-    }
-    
-    static func getInstance() -> DataRepo
-    {
-        return repository
-    }
-    
-    func addVehicle(vehicle: Vehicle)
-    {
-        self.vehicleList.updateValue(vehicle, forKey:
-            vehicle.vehicleIdentificationNumber)
-    }
-    
-    func getVehicle(vin: String) -> Vehicle? {
-        self.vehicleList[vin]
-    }
-    
-    func getAllVehicles() -> [String:Vehicle] {
-        self.vehicleList
-    }
+//   static private var repository = DataRepo()
+//
+//    private lazy var vehicleList = Dictionary <String,Vehicle>()
+//
+//    private init()
+//    {
+//
+//    }
+//
+//    static func getInstance() -> DataRepo
+//    {
+//        return repository
+//    }
+//
+//    func addVehicle(vehicle: Vehicle)
+//    {
+//        self.vehicleList.updateValue(vehicle, forKey:
+//            vehicle.vehicleIdentificationNumber)
+//    }
+//
+//    func getVehicle(vin: String) -> Vehicle? {
+//        self.vehicleList[vin]
+//    }
+//
+//    func getAllVehicles() -> [String:Vehicle] {
+//        self.vehicleList
+//    }
+
 }
